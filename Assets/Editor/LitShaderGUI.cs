44c3867e2b54b8979dfe4ee1191d3727f6102e27
younne0915 <younne@163.com>
2020-03-11 19:07:57 +0800

    gui

---
+++ Assets/Editor/LitShaderGUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class LitShaderGUI : ShaderGUI
+{
+    MaterialEditor editor;
+    Object[] materials;
+    MaterialProperty[] properties;
+
+    public override void OnGUI(
+        MaterialEditor materialEditor, MaterialProperty[] properties
+    )
+    {
+        base.OnGUI(materialEditor, properties);
+
+        editor = materialEditor;
+        materials = materialEditor.targets;
+        this.properties = properties;
+    }
+
+    void SetPassEnabled(string pass, bool enabled)
+    {
+        foreach (Material m in materials)
+        {
+            m.SetShaderPassEnabled(pass, enabled);
+        }
+    }
+
+    bool? IsPassEnabled(string pass)
+    {
+        bool enabled = ((Material)materials[0]).GetShaderPassEnabled(pass);
+        for (int i = 1; i < materials.Length; i++)
+        {
+            if (enabled != ((Material)materials[i]).GetShaderPassEnabled(pass))
+            {
+                return null;
+            }
+        }
+        return enabled;
+    }
+}
--- Assets/My Pipeline/Shaders/Liit.shader
+++ Assets/My Pipeline/Shaders/Liit.shader
@@ -63,4 +63,6 @@
 			ENDHLSL
 		}
     }
+
+	CustomEditor "LitShaderGUI"
 }
