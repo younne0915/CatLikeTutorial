5d95ea968f04b45c2dd3055578c8f444b56203ce
shicheng88 <shicheng.ji@happyelements.com>
2020-03-12 08:39:25 +0800

    shader gui

--- Assets/Editor/LitShaderGUI.cs
+++ Assets/Editor/LitShaderGUI.cs
@@ -8,6 +8,77 @@ public class LitShaderGUI : ShaderGUI
     Object[] materials;
     MaterialProperty[] properties;
 
+    CullMode Cull
+    {
+        set
+        {
+            FindProperty("_Cull", properties).floatValue = (float)value;
+        }
+    }
+
+    BlendMode SrcBlend
+    {
+        set
+        {
+            FindProperty("_SrcBlend", properties).floatValue = (float)value;
+        }
+    }
+
+    BlendMode DstBlend
+    {
+        set
+        {
+            FindProperty("_DstBlend", properties).floatValue = (float)value;
+        }
+    }
+
+    bool ZWrite
+    {
+        set
+        {
+            FindProperty("_ZWrite", properties).floatValue = value ? 1 : 0;
+        }
+    }
+
+    enum ClipMode
+    {
+        Off, On, Shadows
+    }
+
+    ClipMode Clipping
+    {
+        set
+        {
+            FindProperty("_Clipping", properties).floatValue = (float)value;
+            SetKeywordEnabled("_CLIPPING_OFF", value == ClipMode.Off);
+            SetKeywordEnabled("_CLIPPING_ON", value == ClipMode.On);
+            SetKeywordEnabled("_CLIPPING_SHADOWS", value == ClipMode.Shadows);
+        }
+    }
+
+    bool ReceiveShadows
+    {
+        set
+        {
+            FindProperty("_ReceiveShadows", properties).floatValue =
+                value ? 1 : 0;
+            SetKeywordEnabled("_RECEIVE_SHADOWS", value);
+        }
+    }
+
+    RenderQueue RenderQueue
+    {
+        set
+        {
+            foreach (Material m in materials)
+            {
+                m.renderQueue = (int)value;
+            }
+        }
+    }
+
+    bool showPresets;
+
     public override void OnGUI(
         MaterialEditor materialEditor, MaterialProperty[] properties
     )
@@ -17,6 +88,123 @@ public class LitShaderGUI : ShaderGUI
         editor = materialEditor;
         materials = materialEditor.targets;
         this.properties = properties;
+
+        CastShadowsToggle();
+
+        EditorGUILayout.Space();
+        showPresets = EditorGUILayout.Foldout(showPresets, "Presets", true);
+        if (showPresets)
+        {
+            OpaquePreset();
+            ClipPreset();
+            ClipDoubleSidedPreset();
+            FadePreset();
+            FadeWithShadowsPreset();
+        }
+    }
+
+    void FadeWithShadowsPreset()
+    {
+        if (!GUILayout.Button("Fade with Shadows"))
+        {
+            return;
+        }
+        editor.RegisterPropertyChangeUndo("Fade with Shadows Preset");
+        Clipping = ClipMode.Shadows;
+        Cull = CullMode.Back;
+        SrcBlend = BlendMode.SrcAlpha;
+        DstBlend = BlendMode.OneMinusSrcAlpha;
+        ZWrite = false;
+        ReceiveShadows = true;
+        SetPassEnabled("ShadowCaster", true);
+        RenderQueue = RenderQueue.Transparent;
+    }
+
+    void FadePreset()
+    {
+        if (!GUILayout.Button("Fade"))
+        {
+            return;
+        }
+        editor.RegisterPropertyChangeUndo("Fade Preset");
+        Clipping = ClipMode.Off;
+        Cull = CullMode.Back;
+        SrcBlend = BlendMode.SrcAlpha;
+        DstBlend = BlendMode.OneMinusSrcAlpha;
+        ZWrite = false;
+        ReceiveShadows = false;
+        SetPassEnabled("ShadowCaster", false);
+        RenderQueue = RenderQueue.Transparent;
+    }
+
+    void ClipDoubleSidedPreset()
+    {
+        if (!GUILayout.Button("Clip Double-Sided"))
+        {
+            return;
+        }
+        editor.RegisterPropertyChangeUndo("Clip Double-Sided Preset");
+        Clipping = ClipMode.On;
+        Cull = CullMode.Off;
+        SrcBlend = BlendMode.One;
+        DstBlend = BlendMode.Zero;
+        ZWrite = true;
+        ReceiveShadows = true;
+        SetPassEnabled("ShadowCaster", true);
+        RenderQueue = RenderQueue.AlphaTest;
+    }
+
+    void ClipPreset()
+    {
+        if (!GUILayout.Button("Clip"))
+        {
+            return;
+        }
+        editor.RegisterPropertyChangeUndo("Clip Preset");
+        Clipping = ClipMode.On;
+        Cull = CullMode.Back;
+        SrcBlend = BlendMode.One;
+        DstBlend = BlendMode.Zero;
+        ZWrite = true;
+        ReceiveShadows = true;
+        SetPassEnabled("ShadowCaster", true);
+        RenderQueue = RenderQueue.AlphaTest;
+    }
+
+    void OpaquePreset()
+    {
+        if (!GUILayout.Button("Opaque"))
+        {
+            return;
+        }
+        editor.RegisterPropertyChangeUndo("Opague Preset");
+
+        Clipping = ClipMode.Off;
+        Cull = CullMode.Back;
+        SrcBlend = BlendMode.One;
+        DstBlend = BlendMode.Zero;
+        ZWrite = true;
+        ReceiveShadows = true;
+        SetPassEnabled("ShadowCaster", true);
+        RenderQueue = RenderQueue.Geometry;
+    }
+
+    void SetKeywordEnabled(string keyword, bool enabled)
+    {
+        if (enabled)
+        {
+            foreach (Material m in materials)
+            {
+                m.EnableKeyword(keyword);
+            }
+        }
+        else
+        {
+            foreach (Material m in materials)
+            {
+                m.DisableKeyword(keyword);
+            }
+        }
     }
 
     void SetPassEnabled(string pass, bool enabled)
@@ -38,5 +226,24 @@ public class LitShaderGUI : ShaderGUI
             }
         }
         return enabled;
+    }
+
+    void CastShadowsToggle()
+    {
+        bool? enabled = IsPassEnabled("ShadowCaster");
+        if (!enabled.HasValue)
+        {
+            EditorGUI.showMixedValue = true;
+            enabled = false;
+        }
+
+        EditorGUI.BeginChangeCheck();
+        enabled = EditorGUILayout.Toggle("Cast Shadows", enabled.Value);
+        if (EditorGUI.EndChangeCheck())
+        {
+            editor.RegisterPropertyChangeUndo("Cast Shadows");
+            SetPassEnabled("ShadowCaster", enabled.Value);
+        }
+        EditorGUI.showMixedValue = false;
     }
 }
